9d852c9082435c84395c1e661854728f3932c461
emrahelis <emrahelis@gmail.com>
2024-10-23 21:40:23 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+// Türeyen sınıfı bir nesne örneği alalım!!
+
+B be = new B();
+be.Aciklama="Türeyen sınıf";
+
+// b bir sınıf ve a sınıfından türüyor, aynı zamanda a sınıfın açıklama isminde bir propu kalıtım ile b sınıfına geçmiş!!
+
+// bu hiyerarşide, çalışma zamanında nasıl bir bellek hareketlenmesi yaşar!!
+
+B birinci = new B();
---
+++ a.cs
@@ -0,0 +1,13 @@
+public class A{
+
+    public string Aciklama { get; set; }
+    public A()
+    {
+
+    }
+  
+}
+
+// B sonra A sonra B
+// A sonra B
+// sadece B
